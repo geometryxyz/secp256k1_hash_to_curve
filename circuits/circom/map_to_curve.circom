0ebf5b06807830b17f4c00a02e43b132656e965b
Koh Wei Jie <contact@kohweijie.com>
2022-10-01 16:47:19 -0700

    wip - completed 11 / 20 map-to-curve steps

--- circuits/circom/map_to_curve.circom
+++ circuits/circom/map_to_curve.circom
@@ -7,17 +7,22 @@ include "../node_modules/circom-ecdsa/node_modules/circomlib/circuits/comparator
 
 template CMov() {
     // If c is 0, output a. Otherwise, output b. 
-    signal input a;
-    signal input b;
+    signal input a[4];
+    signal input b[4];
     signal input c;
-    signal output out;
+    signal output out[4];
 
-    component mux = Mux1();
-    mux.c[0] <== a;
-    mux.c[1] <== b;
-    mux.s <== c;
+    component mux[4];
+    for (var i = 0; i < 4; i ++) {
+        mux[i] = Mux1();
+        mux[i].c[0] <== a[i];
+        mux[i].c[1] <== b[i];
+        mux[i].s <== c;
+    }
 
-    out <== mux.out;
+    for (var i = 0; i < 4; i ++) {
+        out[i] <== mux[i].out;
+    }
 }
 
 template Inv0() {
@@ -184,6 +189,7 @@ template MapToCurve() {
     signal output x;
     signal output y;
 
+    ///////////////////////////////////////////////////////////////////////////
     // Step 1: tv1 = Z * u^2
     component step1_square = Square();
     for (var i = 0; i < 4; i ++) {
@@ -194,10 +200,128 @@ template MapToCurve() {
         step1_mul.u_squared[i] <== step1_square.out[i];
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 2: tv2 = tv1^2
+    component step2_tv1_sq = Square();
     for (var i = 0; i < 4; i ++) {
-        log(step1_mul.out[i]);
+        step2_tv1_sq.in[i] <== step1_mul.out[i];
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 3: x1 = tv1 + tv2
+    component step3_tv1_plus_tv2 = Add();
+    for (var i = 0; i < 4; i ++) {
+        step3_tv1_plus_tv2.a[i] <== step1_mul.out[i];
+        step3_tv1_plus_tv2.b[i] <== step2_tv1_sq.out[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 4: x1 = inv0(x1)
+    component step4_inv0_x1 = Inv0();
+    for (var i = 0; i < 4; i ++) {
+        step4_inv0_x1.a[i] <== step3_tv1_plus_tv2.out[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 5: e1 = x1 == 0
+    component step5_is_zeroes[4];
+    for (var i = 0; i < 4; i ++) {
+        step5_is_zeroes[i] = IsZero();
+        step5_is_zeroes[i].in <== step4_inv0_x1.out[i];
+    }
+    component step5_e1 = IsEqual();
+    step5_e1.in[0] <== 
+        step5_is_zeroes[0].out +
+        step5_is_zeroes[1].out +
+        step5_is_zeroes[2].out +
+        step5_is_zeroes[3].out;
+    step5_e1.in[1] <== 4;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 6: x1 = x1 + 1
+    component step6_x1_plus_1 = Add();
+    for (var i = 0; i < 4; i ++) {
+        step6_x1_plus_1.a[i] <== step4_inv0_x1.out[i];
+    }
+    step6_x1_plus_1.b[0] <== 1;
+    step6_x1_plus_1.b[1] <== 0;
+    step6_x1_plus_1.b[2] <== 0;
+    step6_x1_plus_1.b[3] <== 0;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 7: x1 = CMOV(x1, c2, e1)    # If (tv1 + tv2) == 0, set x1 = -1 / Z
+    var c2[4] = get_C2();
+    component step7_cmov = CMov();
+    step7_cmov.c <== step5_e1.out;
+    for (var i = 0; i < 4; i ++) {
+        step7_cmov.a[i] <== step6_x1_plus_1.out[i];
+        step7_cmov.b[i] <== c2[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 8: x1 = x1 * c1      # x1 = (-B / A) * (1 + (1 / (Z^2 * u^4 + Z * u^2)))
+    component step8_x1_mul_c1 = Multiply();
+    var c1[4] = get_C1();
+    for (var i = 0; i < 4; i ++) {
+        step8_x1_mul_c1.a[i] <== step7_cmov.out[i];
+        step8_x1_mul_c1.b[i] <== c1[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 9: gx1 = x1^2
+    component step9_gx1 = Square();
+    for (var i = 0; i < 4; i ++) {
+        step9_gx1.in[i] <== step8_x1_mul_c1.out[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 10: gx1 = gx1 + A
+    var a[4] = get_A();
+    component step10_gx1 = Add();
+    for (var i = 0; i < 4; i ++) {
+        step10_gx1.a[i] <== step9_gx1.out[i];
+        step10_gx1.b[i] <== a[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 11: gx1 = gx1 * x1
+    component step11_gx1_mul_x1 = Multiply();
+    for (var i = 0; i < 4; i ++) {
+        step11_gx1_mul_x1.a[i] <== step10_gx1.out[i];
+        step11_gx1_mul_x1.b[i] <== step8_x1_mul_c1.out[i];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 12: gx1 = gx1 + B             # gx1 = g(x1) = x1^3 + A * x1 + B
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 13: x2 = tv1 * x1            # x2 = Z * u^2 * x1
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 14: tv2 = tv1 * tv2
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 15: gx2 = gx1 * tv2           # gx2 = (Z * u^2)^3 * gx1
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 16: e2 = is_square(gx1)
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 17: x = CMOV(x2, x1, e2)    # If is_square(gx1), x = x1, else x = x2
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 18: y2 = CMOV(gx2, gx1, e2)  # If is_square(gx1), y2 = gx1, else y2 = gx2
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 18: y = sqrt(y2)
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 19: e3 = sgn0(u) == sgn0(y)  # Fix sign of y
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Step 20: y = CMOV(-y, y, e3)
 
     x <== 0;
     y <== 0;
+
 }
--- circuits/ts/__tests__/MapToCurve.test.ts
+++ circuits/ts/__tests__/MapToCurve.test.ts
@@ -15,128 +15,139 @@ const u0 = BigInt('8386638881075453792406600069412283052291822895580742641789327
 const u1 = BigInt('40071583224459737250239606232440854032076176808341187421679277989916398099968')
 const u0_registers = bigint_to_array(64, 4, u0)
 const u1_registers = bigint_to_array(64, 4, u1)
+const A = BigInt('28734576633528757162648956269730739219262246272443394170905244663053633733939')
+const B = BigInt('1771')
 const Z = BigInt('115792089237316195423570985008687907853269984665640564039457584007908834671652')
 const Z_registers = bigint_to_array(64, 4, Z)
+const c1 = BigInt('5324262023205125242632636178842408935272934169651804884418803605709653231043')
+const c2 = BigInt('31579660701086235115519359547823974869073632181538335647124795638520591274090')
 
 describe('MapToCurve', () => {
-    it('u ** 2', async () => {
-        const expected_registers: bigint[] = bigint_to_array(64, 4, (u0 * u0) % p)
+    //it('u ** 2', async () => {
+        //const expected_registers: bigint[] = bigint_to_array(64, 4, (u0 * u0) % p)
 
-        const circuit = 'u_squared_test'
-        const circuitInputs = stringifyBigInts({ val: u0_registers })
-        const witness = await genWitness(circuit, circuitInputs)
-        for (let i = 0; i < 4; i ++) {
-            const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
-            expect(out).toEqual(expected_registers[i])
-        }
-    })
+        //const circuit = 'u_squared_test'
+        //const circuitInputs = stringifyBigInts({ val: u0_registers })
+        //const witness = await genWitness(circuit, circuitInputs)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(expected_registers[i])
+        //}
+    //})
 
-    it('Z * u^2', async () => {
-        const u_squared = bigint_to_array(64, 4, (u0 * u0) % p)
-        const expected_registers: bigint[] = bigint_to_array(64, 4, (Z * (u0 * u0)) % p)
-        const circuit = 'z_mul_u_squared_test'
-        const circuitInputs = stringifyBigInts({ u_squared })
-        const witness = await genWitness(circuit, circuitInputs)
-        for (let i = 0; i < 4; i ++) {
-            const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
-            expect(out).toEqual(expected_registers[i])
-        }
-    })
+    //it('Z * u^2', async () => {
+        //const u_squared = bigint_to_array(64, 4, (u0 * u0) % p)
+        //const expected_registers: bigint[] = bigint_to_array(64, 4, (Z * (u0 * u0)) % p)
+        //const circuit = 'z_mul_u_squared_test'
+        //const circuitInputs = stringifyBigInts({ u_squared })
+        //const witness = await genWitness(circuit, circuitInputs)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(expected_registers[i])
+        //}
+    //})
 
-    it('x1 = tv1 + tv2', async () => {
-        const tv1 = (Z * u0 * u0) % p
-        const tv2 = (tv1 * tv1) % p
+    //it('x1 = tv1 + tv2', async () => {
+        //const tv1 = (Z * u0 * u0) % p
+        //const tv2 = (tv1 * tv1) % p
 
-        expect(tv1).toEqual(BigInt('20859123609890259037730945376790481235073009751836911085977936258096472725432'))
-        expect(tv2).toEqual(BigInt('69125977817722673093369867421906949146947726600759336378042760386702683237276'))
+        //expect(tv1).toEqual(BigInt('20859123609890259037730945376790481235073009751836911085977936258096472725432'))
+        //expect(tv2).toEqual(BigInt('69125977817722673093369867421906949146947726600759336378042760386702683237276'))
 
-        const tv1_registers = bigint_to_array(64, 4, tv1)
-        const tv2_registers = bigint_to_array(64, 4, tv2)
-        const expected_registers = bigint_to_array(64, 4, (tv1 + tv2) % p)
+        //const tv1_registers = bigint_to_array(64, 4, tv1)
+        //const tv2_registers = bigint_to_array(64, 4, tv2)
+        //const expected_registers = bigint_to_array(64, 4, (tv1 + tv2) % p)
 
-        const x1_registers = bigint_to_array(64, 4, BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708'))
-        for (let i = 0; i < 4; i ++) {
-            expect(x1_registers[i]).toEqual(expected_registers[i])
-        }
+        //const x1_registers = bigint_to_array(64, 4, BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708'))
+        //for (let i = 0; i < 4; i ++) {
+            //expect(x1_registers[i]).toEqual(expected_registers[i])
+        //}
 
-        const circuit = 'tv2_plus_tv1_test'
-        const circuitInputs = stringifyBigInts({
-            a: tv1_registers,
-            b: tv2_registers,
-        })
-        const witness = await genWitness(circuit, circuitInputs)
-        for (let i = 0; i < 4; i ++) {
-            const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
-            expect(out).toEqual(expected_registers[i])
-        }
-    })
+        //const circuit = 'tv2_plus_tv1_test'
+        //const circuitInputs = stringifyBigInts({
+            //a: tv1_registers,
+            //b: tv2_registers,
+        //})
+        //const witness = await genWitness(circuit, circuitInputs)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(expected_registers[i])
+        //}
+    //})
 
-    it('x1 = inv0(x1)', async () => {
-        const x1 = BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708')
-        const inv_x1 = BigInt('8772949712675871307975074402064985445164432129069910775576927500367828979411')
-        const x1_registers = bigint_to_array(64, 4, x1)
-        const inv_x1_registers = bigint_to_array(64, 4, inv_x1)
-        const circuit = 'inv0_test'
-        const circuitInputs = stringifyBigInts({
-            a: x1_registers,
-        })
-        const witness = await genWitness(circuit, circuitInputs)
-        for (let i = 0; i < 4; i ++) {
-            const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
-            expect(out).toEqual(inv_x1_registers[i])
-        }
-    })
+    //it('x1 = inv0(x1)', async () => {
+        //const x1 = BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708')
+        //const inv_x1 = BigInt('8772949712675871307975074402064985445164432129069910775576927500367828979411')
+        //const x1_registers = bigint_to_array(64, 4, x1)
+        //const inv_x1_registers = bigint_to_array(64, 4, inv_x1)
+        //const circuit = 'inv0_test'
+        //const circuitInputs = stringifyBigInts({
+            //a: x1_registers,
+        //})
+        //const witness = await genWitness(circuit, circuitInputs)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(inv_x1_registers[i])
+        //}
+    //})
 
-    it('cmov', async () => {
-        const circuit = 'cmov_test'
-        const a = BigInt(123)
-        const b = BigInt(456)
-        const circuitInputs = stringifyBigInts({ a, b, c: 0 })
-        const witness = await genWitness(circuit, circuitInputs)
-        const out = BigInt(await getSignalByName(circuit, witness, 'main.out'))
-        expect(out).toEqual(a)
+    //it('cmov', async () => {
+        //const circuit = 'cmov_test'
+        //const a = BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708')
+        //const b = BigInt('456')
+        //const a_array = bigint_to_array(64, 4, a)
+        //const b_array = bigint_to_array(64, 4, b)
 
-        const circuitInputs2 = stringifyBigInts({ a, b, c: 1 })
-        const witness2 = await genWitness(circuit, circuitInputs2)
-        const out2 = BigInt(await getSignalByName(circuit, witness2, 'main.out'))
-        expect(out2).toEqual(b)
-    })
+        //const circuitInputs = stringifyBigInts({ a: a_array, b: a_array, c: 0 })
+        //const witness = await genWitness(circuit, circuitInputs)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(a_array[i])
+        //}
 
-    it('is_square', async() => {
-        const circuit = 'is_square_test'
-        const n = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212823')
-        const expected_sqrt_mod_p = BigInt('26747645647114029413939469563206094538114691466623191143901133893381837269477')
-        expect((expected_sqrt_mod_p * expected_sqrt_mod_p) % p).toEqual(n)
+        //const circuitInputs2 = stringifyBigInts({ a: a_array, b: b_array, c: 1 })
+        //const witness2 = await genWitness(circuit, circuitInputs2)
+        //for (let i = 0; i < 4; i ++) {
+            //const out = BigInt(await getSignalByName(circuit, witness2, 'main.out[' + i.toString() + ']'))
+            //expect(out).toEqual(b_array[i])
+        //}
+    //})
 
-        const expected_sqrt_mod_p_array = bigint_to_array(64, 4, expected_sqrt_mod_p)
+    //it('is_square', async() => {
+        //const circuit = 'is_square_test'
+        //const n = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212823')
+        //const expected_sqrt_mod_p = BigInt('26747645647114029413939469563206094538114691466623191143901133893381837269477')
+        //expect((expected_sqrt_mod_p * expected_sqrt_mod_p) % p).toEqual(n)
 
-        // Test if (expected_sqrt_mod_p ^ 2 == n) mod p - should output 1
-        const n_array = bigint_to_array(64, 4, n)
-        const circuitInputs = stringifyBigInts({ n: n_array, expected_sqrt: expected_sqrt_mod_p_array })
-        const witness = await genWitness(circuit, circuitInputs)
-        const out = BigInt(await getSignalByName(circuit, witness, 'main.out'))
-        expect(out).toEqual(BigInt(1))
+        //const expected_sqrt_mod_p_array = bigint_to_array(64, 4, expected_sqrt_mod_p)
 
-        // Test if (expected_sqrt_mod_p ^ 2 == (n+1)) mod p - should output 0
-        const n_array2 = bigint_to_array(64, 4, n + BigInt(1))
-        const circuitInputs2 = stringifyBigInts({ n: n_array2, expected_sqrt: expected_sqrt_mod_p_array })
-        const witness2 = await genWitness(circuit, circuitInputs2)
-        const out2 = BigInt(await getSignalByName(circuit, witness2, 'main.out'))
-        expect(out2).toEqual(BigInt(0))
-    })
+        //// Test if (expected_sqrt_mod_p ^ 2 == n) mod p - should output 1
+        //const n_array = bigint_to_array(64, 4, n)
+        //const circuitInputs = stringifyBigInts({ n: n_array, expected_sqrt: expected_sqrt_mod_p_array })
+        //const witness = await genWitness(circuit, circuitInputs)
+        //const out = BigInt(await getSignalByName(circuit, witness, 'main.out'))
+        //expect(out).toEqual(BigInt(1))
 
-    it('sgn0()', async() => {
-        const circuit = 'sgn0_test'
-        const odd = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212823')
-        const even = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212822')
+        //// Test if (expected_sqrt_mod_p ^ 2 == (n+1)) mod p - should output 0
+        //const n_array2 = bigint_to_array(64, 4, n + BigInt(1))
+        //const circuitInputs2 = stringifyBigInts({ n: n_array2, expected_sqrt: expected_sqrt_mod_p_array })
+        //const witness2 = await genWitness(circuit, circuitInputs2)
+        //const out2 = BigInt(await getSignalByName(circuit, witness2, 'main.out'))
+        //expect(out2).toEqual(BigInt(0))
+    //})
 
-        for (const val of [odd, even]) {
-            const circuitInputs = stringifyBigInts({ 'in': bigint_to_array(64, 4, val) })
-            const witness = await genWitness(circuit, circuitInputs)
-            const out = BigInt(await getSignalByName(circuit, witness, 'main.out'))
-            expect(out).toEqual(val % BigInt(2))
-        }
-    })
+    //it('sgn0()', async() => {
+        //const circuit = 'sgn0_test'
+        //const odd = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212823')
+        //const even = BigInt('90488382598551863334512914506083217651378280855023645338709486233743148212822')
+
+        //for (const val of [odd, even]) {
+            //const circuitInputs = stringifyBigInts({ 'in': bigint_to_array(64, 4, val) })
+            //const witness = await genWitness(circuit, circuitInputs)
+            //const out = BigInt(await getSignalByName(circuit, witness, 'main.out'))
+            //expect(out).toEqual(val % BigInt(2))
+        //}
+    //})
 
     it('MapToCurve()', async() => {
         const circuit = 'map_to_curve_test'
@@ -144,6 +155,78 @@ describe('MapToCurve', () => {
         const witness = await genWitness(circuit, circuitInputs)
         const x = BigInt(await getSignalByName(circuit, witness, 'main.x'))
         const y = BigInt(await getSignalByName(circuit, witness, 'main.y'))
-        console.log(x, '\n', y)
+
+        // TODO: Step 1 check
+
+        // Step 2 check
+        const step2_tv1_sq = bigint_to_array(64, 4, BigInt('69125977817722673093369867421906949146947726600759336378042760386702683237276'))
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step2_tv1_sq.out[' + i.toString() + ']'))
+            expect(out).toEqual(step2_tv1_sq[i])
+        }
+  
+        // Step 3 check
+        const step3_tv1_plus_tv2 = bigint_to_array(64, 4, BigInt('89985101427612932131100812798697430382020736352596247464020696644799155962708'))
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step3_tv1_plus_tv2.out[' + i.toString() + ']'))
+            expect(out).toEqual(step3_tv1_plus_tv2[i])
+        }
+
+        // Step 4 check
+        const step4_inv0_x1 = bigint_to_array(64, 4, BigInt('8772949712675871307975074402064985445164432129069910775576927500367828979411'))
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step4_inv0_x1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step4_inv0_x1[i])
+        }
+
+        // Step 5 check
+        const out = BigInt(await getSignalByName(circuit, witness, 'main.step5_e1.out'))
+        expect(out).toEqual(BigInt(0))
+
+        // Step 6 check
+        const step6_x1_plus_1 = BigInt('8772949712675871307975074402064985445164432129069910775576927500367828979411') + BigInt(1)
+        const step6_x1_plus_1_array = bigint_to_array(64, 4, step6_x1_plus_1)
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step6_x1_plus_1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step6_x1_plus_1_array[i])
+        }
+
+        // Step 7 check
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step7_cmov.out[' + i.toString() + ']'))
+            expect(out).toEqual(step6_x1_plus_1_array[i])
+        }
+
+        // Step 8 check
+        const step8_x1_mul_c1 = (step6_x1_plus_1 * c1) % p
+        const step8_x1_mul_c1_array = bigint_to_array(64, 4, step8_x1_mul_c1)
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step8_x1_mul_c1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step8_x1_mul_c1_array[i])
+        }
+
+        // Step 9 check
+        const gx1 = (step8_x1_mul_c1 * step8_x1_mul_c1) % p
+        const step9_gx1_array = bigint_to_array(64, 4, gx1)
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step9_gx1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step9_gx1_array[i])
+        }
+
+        // Step 10 check
+        const step10_gx1 = (gx1 + A) % p
+        const step10_gx1_array = bigint_to_array(64, 4, step10_gx1)
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step10_gx1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step10_gx1_array[i])
+        }
+
+        // Step 11 check
+        const step11_gx1_mul_x1 = (step10_gx1 * step8_x1_mul_c1) % p
+        const step11_gx1_mul_x1_array = bigint_to_array(64, 4, step11_gx1_mul_x1)
+        for (let i = 0; i < 4; i ++) {
+            const out = BigInt(await getSignalByName(circuit, witness, 'main.step11_gx1_mul_x1.out[' + i.toString() + ']'))
+            expect(out).toEqual(step11_gx1_mul_x1_array[i])
+        }
     })
 })
