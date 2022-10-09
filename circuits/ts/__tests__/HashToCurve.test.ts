b6b56cfbcc09dfde1b4e8e1cf72821b9f29d9888
Koh Wei Jie <contact@kohweijie.com>
2022-10-09 10:16:03 -0700

    added another test case

--- circuits/ts/__tests__/HashToCurve.test.ts
+++ circuits/ts/__tests__/HashToCurve.test.ts
@@ -1,4 +1,4 @@
-jest.setTimeout(120000)
+jest.setTimeout(360000)
 const crypto = require('crypto')
 const ff = require('ffjavascript')
 const stringifyBigInts = ff.utils.stringifyBigInts
@@ -22,6 +22,21 @@ const test_suites = [
         expected_x: BigInt('0xbac54083f293f1fe08e4a70137260aa90783a5cb84d3f35848b324d0674b0e3a'),
         expected_y: BigInt('0x4436476085d4c3c4508b60fcf4389c40176adce756b398bdee27bca19758d828'),
     },
+    {
+        circuit: 'hash_to_curve_133_test',
+        msg: 'q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
+        expected_x: BigInt('0xe2167bc785333a37aa562f021f1e881defb853839babf52a7f72b102e41890e9'),
+        expected_y: BigInt('0xf2401dd95cc35867ffed4f367cd564763719fbc6a53e969fb8496a1e6685d873'),
+    },
+    // The circuit for the following test case may not work with circom-helper
+    // because of the large amount of JS heap memory it needs to run `snarkjs
+    // r1cs info`.
+    //{
+        //circuit: 'hash_to_curve_517_test',
+        //msg: 'a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        //expected_x: BigInt('0xe3c8d35aaaf0b9b647e88a0a0a7ee5d5bed5ad38238152e4e6fd8c1f8cb7c998'),
+        //expected_y: BigInt('0x8446eeb6181bf12f56a9d24e262221cc2f0c4725c7e3803024b5888ee5823aa6'),
+    //},
 ]
 
 describe('HashToCurve', () => {
