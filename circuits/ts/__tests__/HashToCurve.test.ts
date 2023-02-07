d2fb41390db118212c6d6c218d1e5a0d6164ff56
blakemscurr <blakemscurr@gmail.com>
2023-02-08 12:08:54 +1300

    Revert circom_tester changes.

--- circuits/ts/__tests__/HashToCurve.test.ts
+++ circuits/ts/__tests__/HashToCurve.test.ts
@@ -18,18 +18,18 @@ const test_suites = [
         expected_x: BigInt('0x3377e01eab42db296b512293120c6cee72b6ecf9f9205760bd9ff11fb3cb2c4b'),
         expected_y: BigInt('0x7f95890f33efebd1044d382a01b1bee0900fb6116f94688d487c6c7b9c8371f6'),
     },
-    // {
-    //     circuit: 'hash_to_curve_16_test',
-    //     msg: 'abcdef0123456789',
-    //     expected_x: BigInt('0xbac54083f293f1fe08e4a70137260aa90783a5cb84d3f35848b324d0674b0e3a'),
-    //     expected_y: BigInt('0x4436476085d4c3c4508b60fcf4389c40176adce756b398bdee27bca19758d828'),
-    // },
-    // {
-    //     circuit: 'hash_to_curve_133_test',
-    //     msg: 'q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
-    //     expected_x: BigInt('0xe2167bc785333a37aa562f021f1e881defb853839babf52a7f72b102e41890e9'),
-    //     expected_y: BigInt('0xf2401dd95cc35867ffed4f367cd564763719fbc6a53e969fb8496a1e6685d873'),
-    // },
+    {
+        circuit: 'hash_to_curve_16_test',
+        msg: 'abcdef0123456789',
+        expected_x: BigInt('0xbac54083f293f1fe08e4a70137260aa90783a5cb84d3f35848b324d0674b0e3a'),
+        expected_y: BigInt('0x4436476085d4c3c4508b60fcf4389c40176adce756b398bdee27bca19758d828'),
+    },
+    {
+        circuit: 'hash_to_curve_133_test',
+        msg: 'q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
+        expected_x: BigInt('0xe2167bc785333a37aa562f021f1e881defb853839babf52a7f72b102e41890e9'),
+        expected_y: BigInt('0xf2401dd95cc35867ffed4f367cd564763719fbc6a53e969fb8496a1e6685d873'),
+    },
     // The circuit for the following test case may not work with circom-helper
     // because of the large amount of JS heap memory it needs to run `snarkjs
     // r1cs info`.
@@ -53,18 +53,14 @@ describe('HashToCurve', () => {
             const inputs = generate_inputs(msg)
 
             const circuitInputs = stringifyBigInts(inputs)
-            // const witness = await genWitness(circuit, circuitInputs)
+            const witness = await genWitness(circuit, circuitInputs)
 
-            // for (let i = 0; i < 4; i ++) {
-            //     const out_x = BigInt(await getSignalByName(circuit, witness, 'main.out[0][' + i.toString() + ']'))
-            //     expect(out_x).toEqual(expected_x_array[i])
-            //     const out_y = BigInt(await getSignalByName(circuit, witness, 'main.out[1][' + i.toString() + ']'))
-            //     expect(out_y).toEqual(expected_y_array[i])
-            // }
-            const p = join(__dirname, "../../circom/test", circuit + '.circom')
-            const c = await wasm_tester(p, {"json":true, "sym": true})
-            const w = await c.calculateWitness(circuitInputs)
-            await c.checkConstraints(w)
+            for (let i = 0; i < 4; i ++) {
+                const out_x = BigInt(await getSignalByName(circuit, witness, 'main.out[0][' + i.toString() + ']'))
+                expect(out_x).toEqual(expected_x_array[i])
+                const out_y = BigInt(await getSignalByName(circuit, witness, 'main.out[1][' + i.toString() + ']'))
+                expect(out_y).toEqual(expected_y_array[i])
+            }
         })
     }
 })
