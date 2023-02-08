319495260a70aa96dc424bbbb238a27b4b08ae48
Koh Wei Jie <contact@kohweijie.com>
2023-02-08 14:40:54 +0800

    use circom-tester on the smallest HashToCurve test to check constraints

--- circuits/ts/__tests__/HashToCurve.test.ts
+++ circuits/ts/__tests__/HashToCurve.test.ts
@@ -17,6 +17,7 @@ const test_suites = [
         msg: 'abc',
         expected_x: BigInt('0x3377e01eab42db296b512293120c6cee72b6ecf9f9205760bd9ff11fb3cb2c4b'),
         expected_y: BigInt('0x7f95890f33efebd1044d382a01b1bee0900fb6116f94688d487c6c7b9c8371f6'),
+        check_constraints: true,
     },
     {
         circuit: 'hash_to_curve_16_test',
@@ -60,6 +61,13 @@ describe('HashToCurve', () => {
                 expect(out_x).toEqual(expected_x_array[i])
                 const out_y = BigInt(await getSignalByName(circuit, witness, 'main.out[1][' + i.toString() + ']'))
                 expect(out_y).toEqual(expected_y_array[i])
+            }
+
+            if (suite.check_constraints) {
+                const p = join(__dirname, "../../circom/test", circuit + '.circom')
+                const c = await wasm_tester(p, {"json":true, "sym": true})
+                const w = await c.calculateWitness(circuitInputs)
+                await c.checkConstraints(w)
             }
         })
     }
